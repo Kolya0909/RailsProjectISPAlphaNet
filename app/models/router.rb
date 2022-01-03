fcf1c6caee0db70a60ad47bf10e15154c027abac
Kolya <bond410@ukr.net>
2022-01-03 20:58:59 +0200

    updating_adding_router

--- app/models/router.rb
+++ app/models/router.rb
@@ -9,6 +9,8 @@ class Router < ApplicationRecord
         
                                             #Должна быть проверка является ли тот юзер которому мы додоем роутер админом?
 
+        if team === 'admin@ukr.net'
+
         flag = false
         
         all_routers_team.each do |router|   # Проверяем есть ли у нашего юзера тот роутер что нам нужно добавить
@@ -26,6 +28,61 @@ class Router < ApplicationRecord
         
         user.routers = all_routers_team
         user.save
+
+    else # Если получатель роутера не является админом
+
+            admin = User.find_by(email: 'admin@ukr.net')
+            user_no_admin = User.find_by(email: team)    #Получаем юзера
+            all_routers_team = user_no_admin.routers 
+            flag = false
+           
+            all_routers_team.each do |router|
+
+                if router[:name] == router_name&&router[:model] == router_model # Проверяем есть ли у нашего юзера тот роутер что нам нужно добавить
+
+                    old_router_count = router[:count]
+                    new_router_count = old_router_count.to_i + router_count.to_i
+                    router[:count] = new_router_count
+                    flag = true
+
+                    admin.routers.each do |router| # Ищем нужный роутер в админа и делаем операцию
+
+                        if router[:name] == router_name&&router[:model] == router_model
+
+                            router[:count] = router[:count].to_i - router_count.to_i
+
+                        end
+
+                    end
+                    user_no_admin.routers = all_routers_team
+                    admin.save
+                end  
+            end
+                if flag == false
+
+                    user_no_admin.routers.push({:name=>router_name,:model=>router_model,:count=>router_count}) #Если первая проверка не прошла значит у нас еще нет такого роутера и мы его добавляем к юзеру.
+                    user_no_admin.save
+                    admin.routers.each do |router| # Ищем нужный роутер в админа и делаем операцию
+
+                        if router[:name] == router_name&&router[:model] == router_model
+
+                            router[:count] = router[:count].to_i - router_count.to_i
+
+                        end
+
+                    end
+                    admin.save
+
+                end 
+
+                
+                user_no_admin.save
+                
+
+        end
+
+
+        
         
     end
     
