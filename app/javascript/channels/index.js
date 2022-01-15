fa22dfcaa606a40a4c51a218ea862b8b053c9b1a
Kolya <bond410@ukr.net>
2022-01-16 00:17:28 +0200

    updated_work

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -6,20 +6,4 @@ channels.keys().forEach(channels)
 
 
 
-window.onload = () =>{
 
-    const useRouterBtn = document.querySelector('.check__router')
-    const formCloseWork = document.querySelector('.close__work-form')
-
-    useRouterBtn.addEventListener('click', ()=>{
-        if(useRouterBtn.checked){
-            
-            formCloseWork.style.opacity = '1'
-            formCloseWork.style.height = '100%'
-        }
-        else{
-            formCloseWork.style.opacity = '0'
-            formCloseWork.style.height = '0'
-        }
-    })
-}
